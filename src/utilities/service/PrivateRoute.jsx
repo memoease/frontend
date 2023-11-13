b9509f06b614258ab02de9bcdd1a148006d7abb2
Alsulayman <radwansulimann@gmail.com>
2023-11-13 16:13:23 +0100

    Outlet importiert

--- src/utilities/service/PrivateRoute.jsx
+++ src/utilities/service/PrivateRoute.jsx
@@ -1,4 +1,4 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 
 export default function PrivateRoute() {
