5066435799f86f92e841b60f3354b1f20b9b8002
alisawiali <alisawiali70@gmail.com>
2023-11-14 11:15:05 +0100

    Updte logout in setting plus stling einheitlicht.

--- src/components/Setting/Setting.jsx
+++ src/components/Setting/Setting.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../css/setting.scss";
+import { NavLink } from "react-router-dom";
 const Setting = () => {
   return (
     <div>
@@ -10,25 +11,30 @@ const Setting = () => {
             <div className="">
               <label htmlFor="username">
                 username
-                <input type="text" name="username" id="username" />
+                <input type="text" name="username" id="username" required />
               </label>
             </div>
             <div className="">
               <label htmlFor="password">
                 old password
-                <input type="password" name="password" id="password" />
+                <input type="password" name="password" id="password" required />
               </label>
             </div>
             <div className="">
               <label htmlFor="password">
                 New password
-                <input type="password" name="password" id="password" />
+                <input type="password" name="password" id="password" required />
               </label>
             </div>
             <div className="setting_btn">
               <button type="submit">save</button>
             </div>
           </form>
+        </div>
+        <div className="logoutBtn">
+          <NavLink className="linkLogout" to="/logout">
+            Log out
+          </NavLink>
         </div>
       </div>
     </div>
