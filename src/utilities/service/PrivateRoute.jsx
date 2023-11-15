b7e1c630fc25f285632dde4dfcb5822fcd1354f6
SiminNaghi <simin.naghi@outlook.com>
2023-11-15 10:12:29 +0100

    restore

--- src/utilities/service/PrivateRoute.jsx
+++ src/utilities/service/PrivateRoute.jsx
@@ -1,28 +1,12 @@
-<<<<<<< HEAD
-import { useAuth } from "../../utilities/hooks/useAuth.jsx";
-
-export default function PrivateRoute() {
-  const { authorized } = useAuth();
-
-  // if (isLoading) {
-  //     return (
-  //         <h2>loading...</h2>
-  //     )
-  // };
-
-  return authorized ? <Outlet /> : <Navigate to="/" />;
-}
-=======
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 
 export default function PrivateRoute() {
-    const { authorized, loading } = useAuth();
+  const { authorized, loading } = useAuth();
 
-    if (loading) {
-        return <h2>loading...</h2>;
-    }
+  if (loading) {
+    return <h2>loading...</h2>;
+  }
 
-    return authorized ? <Outlet /> : <Navigate to="/" />;
-};
->>>>>>> bcb3f2d60f9ff4c7af0465d97225792663c36277
+  return authorized ? <Outlet /> : <Navigate to="/" />;
+}
