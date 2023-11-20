3d82ec2bc61a6f8d725fa7918b3205ef1620f7dc
Alsulayman <radwansulimann@gmail.com>
2023-11-21 00:30:18 +0100

    neue Syle-carousel

--- src/components/DiscoverCarousel/CarouselData.jsx
+++ src/components/DiscoverCarousel/CarouselData.jsx
@@ -5,7 +5,7 @@ const CarouselData = ({ item, id }) => {
     <div>
       <div className="cards" key={id}>
         <div className="DiscoverCardsContent">
-          <h3>{item.name}</h3>
+          <h1 className="title">{item.name}</h1>
           <button>{item.card}</button>
           <p className="discropt-created">{item.description}</p>
           <p>{item.text}</p>
