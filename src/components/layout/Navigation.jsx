370ff2504b20367cdea2688c2f303c3e1bd9f4e9
djoetker <jansiede@posteo.de>
2023-11-23 11:25:57 +0100

    path dashboard update

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -18,71 +18,71 @@ const Navigation = () => {
       console.error("Logout error:", error);
     }
   };
-const menuHandel = () => {
-  setMenuOpen(!menuOpen);
-};
-return (
-  <div className="navigation">
-    <div className="toNavigation-list">
-      <NavLink className="logo" to="/">
-        <h2 className="title">MemoEase</h2>
-      </NavLink>
-      <div className="menu" onClick={menuHandel}>
-        <span></span>
-        <span></span>
-        <span></span>
+  const menuHandel = () => {
+    setMenuOpen(!menuOpen);
+  };
+  return (
+    <div className="navigation">
+      <div className="toNavigation-list">
+        <NavLink className="logo" to="/">
+          <h2 className="title">MemoEase</h2>
+        </NavLink>
+        <div className="menu" onClick={menuHandel}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <ul className={menuOpen ? "open" : ""}>
+          <li>
+            <NavLink className="navList" to="/home">
+              Home
+            </NavLink>
+          </li>
+
+          {!authorized ? (
+            <li>
+              <NavLink className="navList" to="/about">
+                About
+              </NavLink>
+            </li>
+          ) : (
+            <li>
+              <NavLink className="navList" to="/setting">
+                Setting
+              </NavLink>
+            </li>
+          )}
+
+          <li>
+            <NavLink className="navList" to="/contact">
+              Contact
+            </NavLink>
+          </li>
+
+          {!authorized ? (
+            <li className="btn-Head">
+              <NavLink to="/login" className="btnLoginin">
+                Login
+              </NavLink>
+              <NavLink to="/register" className="btnRegister">
+                Register
+              </NavLink>
+            </li>
+          ) : (
+            <li className="btn-Head">
+              <NavLink to="/" className="btnLoginin" onClick={logoutHandler}>
+                Logout
+              </NavLink>
+              <NavLink to="/dashboard" className="btnRegister">
+                Dashboard
+              </NavLink>
+            </li>
+          )}
+        </ul>
       </div>
-      <ul className={menuOpen ? "open" : ""}>
-        <li>
-          <NavLink className="navList" to="/home">
-            Home
-          </NavLink>
-        </li>
-
-        {!authorized ? (
-          <li>
-            <NavLink className="navList" to="/about">
-              About
-            </NavLink>
-          </li>
-        ) : (
-          <li>
-            <NavLink className="navList" to="/setting">
-              Setting
-            </NavLink>
-          </li>
-        )}
-
-        <li>
-          <NavLink className="navList" to="/contact">
-            Contact
-          </NavLink>
-        </li>
-
-        {!authorized ? (
-          <li className="btn-Head">
-            <NavLink to="/login" className="btnLoginin">
-              Login
-            </NavLink>
-            <NavLink to="/register" className="btnRegister">
-              Register
-            </NavLink>
-          </li>
-        ) : (
-          <li className="btn-Head">
-            <NavLink to="/" className="btnLoginin" onClick={logoutHandler}>
-              Logout
-            </NavLink>
-            <NavLink to="/DashBoard_login" className="btnRegister">
-              Dashboard
-            </NavLink>
-          </li>
-        )}
-      </ul>
+      <Outlet />
     </div>
-    <Outlet />
-  </div>
-);
+  );
 };
 
 export default Navigation;
