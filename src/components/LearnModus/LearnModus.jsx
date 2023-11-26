92532e3bfc488e5b7f217fe54e090272fe2b05b4
SiminNaghi <simin.naghi@outlook.com>
2023-11-26 08:42:37 +0100

    No change change

--- src/components/LearnModus/LearnModus.jsx
+++ src/components/LearnModus/LearnModus.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "../../css/LearnModus.scss";
 import FlipCards from "../FlipCards/FlipCards";
+
 import {
   startLearnSession,
   updateSessionCardToLearned,
