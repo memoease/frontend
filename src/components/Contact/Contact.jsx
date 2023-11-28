16e4fad8ee14c3104a2f29ba56353d992b324f54
alisawiali <alisawiali70@gmail.com>
2023-11-28 16:13:00 +0100

    Update contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
+import "../../css/contact.scss";
 const Contact = () => {
+  const [showContent, setShowContent] = useState(false);
+
+  const handleContentNavClick = () => {
+    setShowContent(!showContent);
+  };
+  const handleLinkClick = () => {
+    setShowContent(true); // Öffne den Content-Bereich, wenn auf den Link geklickt wird
+  };
+
   return (
-    <div>
-      <h1 className="text-center">Contact Us</h1>
-      <p></p>
+    <div className="contact_repper">
+      <div className="wrpper-contact">
+        <h1 className="text-center">Contact Us</h1>
+        <button className="btn" onClick={handleContentNavClick}>
+          Click my
+        </button>
+        <div className={`content ${showContent ? "show" : ""}`}>
+          <p>
+            Just like you, our hunger for knowledge knows no bounds. We eagerly
+            welcome your thoughts, whether they're comments, opinions, or
+            expressions of appreciation. Your input fuels our passion for
+            learning. Feel free to drop us a line; we can't wait to engage with
+            your insights and ideas.
+          </p>
+
+          <a href="mailto:memoease.team@gmail.com" onClick={handleLinkClick}>
+            E-Mail: write to us
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
