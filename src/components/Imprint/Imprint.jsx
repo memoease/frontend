d42105d0c00474a061d8774d6c4c71ac2e70227a
SiminNaghi <simin.naghi@outlook.com>
2023-11-29 09:12:50 +0100

    add imprint

---
+++ src/components/Imprint/Imprint.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Imprint = () => {
+  return (
+    <div>
+      <h2>Imprint</h2>
+
+      <p>
+        <strong>Information according to § 5 TMG:</strong>
+      </p>
+      <p>
+        Operator and contact: MemoEase Developers <br />
+        E-mail address:{" "}
+        <a href="mailto:memoease.team@gmail.com">memoease.team@gmail.com</a>
+      </p>
+
+      <p>
+        <strong>
+          Responsible for journalistic-editorial content according to § 55 II
+          RStV:
+        </strong>
+      </p>
+      <p>MemoEase Developers</p>
+
+      <p>
+        <strong>Images and graphics:</strong>
+      </p>
+      <p>Source details for images and graphics used: Freepik</p>
+    </div>
+  );
+};
+
+export default Imprint;
