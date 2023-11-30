26a11948273fb23c624c0d25868c97bba051ec8d
djoetker <jansiede@posteo.de>
2023-11-30 14:59:45 +0100

    public / private button

--- src/components/Edit-set/CardInfo.jsx
+++ src/components/Edit-set/CardInfo.jsx
@@ -1,20 +1,44 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { updateSetInfoById } from "../../utilities/service/api";
 import Description from "./Description";
 
 
 const CardInfo = ({ setId, flashcards, setNewCardAdded, newCardAdded }) => {
     const navigate = useNavigate();
-
+    const [setAccess, setSetAccess] = useState(flashcards.isPublic);
 
     const toSession = () => {
         navigate(`/session/${setId}`);
     };
 
+    const changeSetAccess = async () => {
+        try {
+            const data = {
+                isPublic: !flashcards.isPublic
+            };
+            const response = await updateSetInfoById(setId, data);
+            setNewCardAdded(!newCardAdded);
+            setSetAccess(response.isPublic);
+        } catch (error) {
+            console.error(error);
+        };
+    };
 
-    console.log("flashcard:", flashcards);
     return (
         <div className="cardEdit">
             <Description description={flashcards.description} setId={setId} setNewCardAdded={setNewCardAdded} newCardAdded={newCardAdded} />
+            <section className="set-access">
+                <p>Who can see this set:</p>
+                <p>{setAccess ? "Everybody can see this set." : "Only you."}</p>
+                {
+                    setAccess ?
+                        <button onClick={changeSetAccess}> Change to private</button>
+                        :
+                        <button onClick={changeSetAccess}> Change to public</button>
+
+                }
+            </section>
             <>
                 {
                     (!flashcards.session || flashcards.session.toLearn.length === 0) ?
