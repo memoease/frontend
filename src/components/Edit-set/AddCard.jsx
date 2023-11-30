77c8eb629605363c584617c0c81f3ba3d505a7f0
alisawiali <alisawiali70@gmail.com>
2023-11-30 14:24:05 +0100

    .... Update Responsev create

--- src/components/Edit-set/AddCard.jsx
+++ src/components/Edit-set/AddCard.jsx
@@ -1,51 +1,60 @@
-import { useState } from "react"
+import { useState } from "react";
 import { Form } from "react-router-dom";
 import { useCards } from "../../utilities/hooks/useCards";
 import { postNewCardToSet } from "../../utilities/service/api";
 
-
 export default function AddCard({ setId, setNewCardAdded, newCardAdded }) {
-    const { updateSetsByUser } = useCards();
-    const [entry, setEntry] = useState({
-        question: "",
-        answer: ""
+  const { updateSetsByUser } = useCards();
+  const [entry, setEntry] = useState({
+    question: "",
+    answer: "",
+  });
+  const styleUnderline = {
+    borderBottom: "2px solid black",
+  };
+
+  const changeHandler = (evt) => {
+    setEntry((prev) => ({
+      ...prev,
+      [evt.target.name]: evt.target.value,
+    }));
+  };
+
+  const saveEntry = async (evt) => {
+    evt.preventDefault();
+    await postNewCardToSet(setId, entry);
+    setNewCardAdded(!newCardAdded);
+    await updateSetsByUser();
+    setEntry({
+      question: "",
+      answer: "",
     });
-    const styleUnderline = {
-        borderBottom: "2px solid black"
-    };
+  };
 
-    const changeHandler = (evt) => {
-        setEntry(prev => (
-            {
-                ...prev,
-                [evt.target.name]: evt.target.value
-            }
-        ));
-    };
+  return (
+    <form>
+      <div className="inputCards">
+        <input
+          type="text"
+          name="question"
+          value={entry.question}
+          onChange={changeHandler}
+          required
+          style={styleUnderline}
+        />
+        <input
+          type="text"
+          name="answer"
+          value={entry.answer}
+          onChange={changeHandler}
+          style={styleUnderline}
+          required
+        />
 
-    const saveEntry = async (evt) => {
-        evt.preventDefault();
-        await postNewCardToSet(setId, entry);
-        setNewCardAdded(!newCardAdded);
-        await updateSetsByUser()
-        setEntry({
-            question: "",
-            answer: ""
-        });
-    };
-
-
-
-    return (
-        <form >
-            <div className="inputCards">
-                <input type="text" name="question" value={entry.question} onChange={changeHandler} required style={styleUnderline} />
-                <input type="text" name="answer" value={entry.answer} onChange={changeHandler} style={styleUnderline} required />
-
-                <button className="EditBtn" onClick={saveEntry}>
-                    Save
-                </button>
-            </div>
-        </form>
-    );
-};
+        <button className="EditBtn" onClick={saveEntry}>
+          Save
+        </button>
+      </div>
+    </form>
+  );
+}
