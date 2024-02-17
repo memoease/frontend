9ab12e1c12a3364fea2c8796c1e490ea586a7b48
SiminNaghi <simin.naghi@outlook.com>
2024-02-17 17:38:13 +0100

    timeout extended

--- src/utilities/service/api.js
+++ src/utilities/service/api.js
@@ -4,7 +4,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
-  timeout: 10000,
+  timeout: 60000,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
